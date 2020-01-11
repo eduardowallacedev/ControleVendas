d2a9341b18e9f7a1f12b4ac93ae29db001020e7c
eduardowallacedev <eduardo.wallace@outlook.com.br>
2020-01-11 18:20:26 -0300

    Criação da classe ConnectionFactory

---
+++ Documents/NetBeansProjects/ControleVendas/src/main/java/br/com/controlevendas/jdbc/ConnectionFactory.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.controlevendas.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author eduardowallacedev <eduardo.wallace@outlook.com.br>
+ */
+public class ConnectionFactory {
+    
+    public Connection getConnection(){
+        
+        try {
+            
+            return DriverManager.getConnection("jdbc:mysql://127.0.0.1/bdvendas","usuario","123");
+                    
+                    
+        } catch (Exception error) {
+            throw new RuntimeException(error);
+        }
+ 
+            
+    }
+    
+}
