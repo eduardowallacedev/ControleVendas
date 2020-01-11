c5312b4f4491c212582fe47aeb02dbd753ede6ce
eduardowallacedev <eduardo.wallace@outlook.com.br>
2020-01-11 18:00:19 -0300

    Criacao da classe Cliente com os devidos atributos

---
+++ Documents/NetBeansProjects/ControleVendas/src/main/java/br/com/controlevendas/model/Clientes.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.controlevendas.model;
+
+/**
+ *
+ * @author Eduardo
+ */
+public class Clientes {
+    
+    //Attributes
+    
+    private int id;
+    private String nome;
+    private String rg;
+    private String cpf;
+    private String email;
+    private String telefone;
+    private String celular;
+    private String cep;
+    private String endereco;
+    private int numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    
+    //Getters and Setters
+    
+    //Methods
+    
+}
