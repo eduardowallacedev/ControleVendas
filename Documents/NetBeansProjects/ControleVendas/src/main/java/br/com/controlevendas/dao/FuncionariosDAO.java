d0811ac99ecf2d9e050cb2c1a41d307e1a4ec632
eduardowallacedev <eduardo.wallace@outlook.com.br>
2020-02-08 13:53:09 -0300

    Adicionado metodo alterar funcionario a classe DAO

--- Documents/NetBeansProjects/ControleVendas/src/main/java/br/com/controlevendas/dao/FuncionariosDAO.java
+++ Documents/NetBeansProjects/ControleVendas/src/main/java/br/com/controlevendas/dao/FuncionariosDAO.java
@@ -6,6 +6,7 @@
 package br.com.controlevendas.dao;
 
 import br.com.controlevendas.jdbc.ConnectionFactory;
+import br.com.controlevendas.model.Clientes;
 import br.com.controlevendas.model.Funcionarios;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -106,5 +107,70 @@ public class FuncionariosDAO {
         }
     }
     
+     //Method atualiza o Funcionarios
+    public void alterarFuncionarios(Funcionarios obj){
+        
+         try {
+            //Cria a lista
+            List<Funcionarios> lista = new ArrayList<>();
+            //Criar o comando sql, organizar e executar a sql
+            String sql = "update tb_funcionarios "
+                    + "set nome=?,rg=?,cpf=?,email=?,senha=?,cargo=?,nivel_acesso=?,telefone=?,celular=?,cep=?,endereco=?,numero=?,cidade=?,bairro=?,complemento=?,estado=? "
+                    + "where id=?";
+            try ( // Conectar com o DB e organizar o comando sql
+                    PreparedStatement stmt = con.prepareStatement(sql)) {
+                stmt.setString(1,obj.getNome());
+                stmt.setString(2,obj.getRg());
+                stmt.setString(3,obj.getCpf());
+                stmt.setString(4,obj.getEmail());
+                stmt.setString(5,obj.getSenha());
+                stmt.setString(6,obj.getCargo());
+                stmt.setString(7,obj.getNivelAcesso());
+                stmt.setString(8,obj.getTelefone());
+                stmt.setString(9,obj.getCelular());
+                stmt.setString(10,obj.getCep());
+                stmt.setString(11,obj.getEndereco());
+                stmt.setInt(12,obj.getNumero());
+                stmt.setString(13,obj.getComplemento());
+                stmt.setString(14,obj.getBairro());
+                stmt.setString(15,obj.getCidade());
+                stmt.setString(16,obj.getEstado());
+                stmt.setInt(17,obj.getId());
+                
+                // Executar o comando sql
+                stmt.execute();
+            }
+            
+            JOptionPane.showMessageDialog(null, "Funcionarios atualizado com sucesso!");
+                              
+        } catch (SQLException error) {
+          JOptionPane.showMessageDialog(null, "Erro: "+ error);                        
+        }
+        
+    }
     
+    //Method exclui um Cliente
+    public void excluirCliente(Clientes obj){
+                  
+        try {
+            // Prepara o comando sql
+            String sql = "delete from tb_clientes where id=?";
+            
+            try ( // Conectar com o DB e organizar o comando sql
+                    PreparedStatement stmt = con.prepareStatement(sql)) {
+                stmt.setInt(1,obj.getId());
+                
+                // Executar o comando sql
+                stmt.execute();
+                stmt.close();
+            }
+            
+            JOptionPane.showMessageDialog(null, "Cliente excluído com sucesso!");
+                              
+        } catch (SQLException error) {
+          JOptionPane.showMessageDialog(null, "Erro: "+ error);                        
+        }
+    
+        
+    }
 }
